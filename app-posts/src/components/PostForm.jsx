930ae6e038cf9925215c138abd3519106f5efed6
DiMykhailyk <di.mykhailykk@gmail.com>
2024-11-19 13:44:31 +0200

    refactoring

--- app-posts/src/components/PostForm.jsx
+++ app-posts/src/components/PostForm.jsx
@@ -1,40 +1,39 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import MyInput from "./UI/input/MyInput";
 import MyButton from "./UI/button/MyButton";
 
-const PostForm = ({create}) => {
-  const [post, setPost] = useState({title: '', body: ''})
+const PostForm = ({ create }) => {
+  const [post, setPost] = useState({ title: '', body: '' })
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
 
   const addNewPost = (e) => {
     e.preventDefault()
-    // setPosts([...posts, {...post, id: Date.now()}]);
 
     const newPost = {
-        ...post, 
-        id: Date.now()
+      ...post,
+      id: Date.now()
     }
     create(newPost)
-    setPost({title: '', body: ''})
+    setPost({ title: '', body: '' })
   }
 
-    return (
-        <form>
-        <MyInput
-          value={post.title}
-          onChange={e => setPost({...post, title: e.target.value})}
-          type="text"
-          placeholder="Name"
-        />
-        <MyInput
-          value={post.body}
-          onChange={e => setPost({...post, body: e.target.value})}
-          type="text"
-          placeholder="Description" />
-        <MyButton onClick={addNewPost}>Make post</MyButton>
-      </form>
-    )
+  return (
+    <form>
+      <MyInput
+        value={post.title}
+        onChange={e => setPost({ ...post, title: e.target.value })}
+        type="text"
+        placeholder="Name"
+      />
+      <MyInput
+        value={post.body}
+        onChange={e => setPost({ ...post, body: e.target.value })}
+        type="text"
+        placeholder="Description" />
+      <MyButton onClick={addNewPost}>Make post</MyButton>
+    </form>
+  )
 }
 
 export default PostForm;
